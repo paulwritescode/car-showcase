a4cd0ac1dd3272c1959fb7182ec2f02f73525fe9
moon😏 <134926047+paulwritescode@users.noreply.github.com>
2024-10-07 14:01:30 +0300

    new init

---
+++ components/index.ts
@@ -0,0 +1,19 @@
+// use this as a file to export file then export them
+
+import Hero from './Hero'
+import CustomButton from './CustomButton'
+import Footer from './Footer'
+import Navbar from './Navbar'
+import CustomFilter from './CustomFilter'
+import SearchBar from './SearchBar'
+import SearchManufacturer from './SearchManufacturer'
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
+}
